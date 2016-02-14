e020f3aaba18c5210a9794bacb6d6e267d852730
Victor Hugo <victorhugo.avila.cl@gmail.com>
2016-02-14 15:25:31 -0300

    improvements

--- setup.sh
+++ setup.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-# EasyPoint script to setting up Debian web server
+# EasyPoint script to setting up Debian Web Server with LAMP5.6
 
 echo "**************************************************************"
 echo "Initializing script..."
@@ -27,7 +27,10 @@ echo "Installing rpm..."
 sudo apt-get -y install rpm
 echo "Installing debconf..."
 sudo apt-get install debconf
+echo "Installing GIT..."
+sudo apt-get install git
 
+#Since using gcloud ssh ftp VSFTP is deprecated
 #echo "Installing vsftpd"
 #sudo apt-get install vsftpd
 #sudo systemctl enable vsftpd.service
@@ -46,7 +49,7 @@ export PATH="$PATH:/usr/sbin/"
 while true; do
     read -p "Do you wish to deploy DEV or PROD enviroment? (test/prod) " yn
     case $yn in
-        [test]* ) option=0;break;;
+        [dev]* ) option=0;break;;
         [prod]* ) option=1; break;;
         * ) echo "Please answer yes or no.";;
     esac
@@ -58,7 +61,6 @@ if [ $option = 1 ]; then
   sudo a2dissite default
   sudo rm -r /var/www/html/index.*
   sudo a2dismod autoindex -f
-  #sudo service apache2 restart
   sudo apachectl restart
 else
   echo "Deploying development enviroment..."
@@ -67,17 +69,31 @@ fi
 echo "Preparing folders authorization"
 sudo chmod 555 /var/www/
 sudo chmod 777 /var/www/html
-if [ $option = 0 ]; then
-  sudo touch /var/www/html/index.html
-fi
 
-echo "**************************************************************"
-echo "Installing MySQL..."
-echo "**************************************************************"
-sudo apt-get install -y mysql-server php5-mysql
-sudo mysql_secure_installation
-sudo systemctl enable mysql.service
-sudo systemctl restart mysql.service
+#echo "Preparing some test"
+#if [ $option = 0 ]; then
+#  sudo touch /var/www/html/index.html
+#fi
+#sudo touch /var/www/html/index.html
+#sudo printf '%s\n' '<html><body><h1>I&#39m Running!</h1><br><h4>Test provided by: victorhugo.avila@easy-point.com</h4></body></html>' >> /var/www/html/index.html
+
+while true; do
+    read -p "Do you wish to install MySQL?" yn
+    case $yn in
+        [y]* ) option=0;break;;
+        [n]* ) option=1; break;;
+        * ) echo "Please answer yes or no.";;
+    esac
+done
+if [ $option = 1 ]; then
+  echo "**************************************************************"
+  echo "Installing MySQL..."
+  echo "**************************************************************"
+  sudo apt-get install -y mysql-server php5-mysql
+  sudo mysql_secure_installation
+  sudo systemctl enable mysql.service
+  sudo systemctl restart mysql.service
+fi
 
 echo "**************************************************************"
 echo "Installing phpMyAdmin..."
@@ -87,13 +103,14 @@ sudo apt-get -y install phpmyadmin
 echo "**************************************************************"
 echo "Installing PHP..."
 echo "**************************************************************"
-sudo apt-get -y install php5-common libapache2-mod-php5 php5-cli php5-json php5-curl
+sudo apt-get -y install php5 php5-common libapache2-mod-php5 php5-cli php5-json php5-mcrypt php5-curl php5-gd
+
 sudo service apache2 restart
 sudo touch /var/www/html/info.php
 sudo chmod 777 /var/www/html/info.php
-if [ $option = 0 ]; then
-  sudo printf '%s\n' '<?php phpinfo(); ?>' >> /var/www/html/info.php
-fi
+#if [ $option = 0 ]; then
+sudo printf '%s\n' '<?php phpinfo(); ?>' >> /var/www/html/info.php
+#fi
 
 echo "**************************************************************"
 echo "Installing composer..."
